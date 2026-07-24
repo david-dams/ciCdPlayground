df3c0676e3c9c19fd89b15ea46f78e81f64b122a
David Dams <david.dams@tngtech.com>
2026-07-24 13:54:30 +0200

    feat: intro

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ new App({
   target: document.body,
   props: {
     // What's your name?
-    name: "James Butler",
+    name: "David Dams",
     // In the following fiels you can either give a single string,
     // or an array of bullet points
 
     // What do you associate with the term 'CI/CD'?
-    associations: ["Ehm, what?"],
-    // Which CI/CD tools do you use in your project?
-    tools: "Jenkins, maybe?",
+      associations: ["Continuous Integration / Deployment ;)"],
+      // Which CI/CD tools do you use in your project?
+      tools: "tekton",
     // What do you want to learn in this workshop?
-    expectations: ["what you are talking about"],
+      expectations: ["learn more about jenkins / ci/cd philosophy in general"],
   },
 });
